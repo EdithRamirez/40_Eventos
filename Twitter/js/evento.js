24d34ade29d44629c9f62ecec1e4a94e974a2056
EdithRamirez <ediths.ramirez07@gmail.com>
2016-10-21 16:03:01 -0500

    faltaron estilos, pero el ejercico ya elimina, agrega

--- Twitter/js/evento.js
+++ Twitter/js/evento.js
@@ -1,30 +1,48 @@
 function agregarText() {
+	//los valores de textarea se guardan en la variable addLista
 	var addLista = document.getElementById("tarea").value;
+	//Crea un nodo tipo texto
 	var texto = document.createTextNode(addLista);
-	var newLista = document.createElement("label");
-	var checkbox = document.createElement("input");
-
+	
+	//comprueba si textarea esta vacio
 	if (addLista == "") {
     	alert("No se puede agregar texto en blanco");
  	} else {
  		//atributos del label e input de checkbox
-		newLista.setAttribute("class","center-block input-checkbox");
-		var inputs = document.getElementsByClassName("input-checkbox");
+		var newLista = document.createElement("label");
+		newLista.setAttribute("class","center-block panel gris");
 
+
+		var checkbox = document.createElement("input");
 		checkbox.setAttribute("type","checkbox");
 
+		var span = document.createElement("span");
+		span.setAttribute("class","fa fa-trash rojo");
 
 		newLista.appendChild(checkbox);
 		newLista.appendChild(texto);
+		newLista.appendChild(span);
+
 		document.getElementById("agregarText").appendChild(newLista);	
 
-		addLista = document.getElementById("tarea").value = "";
-
-
-		newLista.onclick = function () {
-			this.parentElement.removeChild(this);
-		};
-
+		//esta linea limpia lo que se tiene en textarea cuando se da clic en boton agregar
+		addLista = document.getElementById("tarea").value = ""; 
     }
 
+
+
+    //elimina al dar clic al span de basura
+	span.onclick = function(){
+		newLista.parentElement.removeChild(newLista);
+	}
+
+
+	//valida si se da checked al input y le agrega la clase de linea
+	checkbox.onclick = function () {
+		if(checkbox.checked == true){
+			texto.parentElement.classList.add("linea");
+		}else{
+			texto.parentElement.classList.remove("linea");
+		}
+	}
 }
