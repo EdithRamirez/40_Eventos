e59486840642c57e9a290f367dc61510ab33422b
EdithRamirez <ediths.ramirez07@gmail.com>
2016-10-24 14:52:40 -0500

    ejercicio trello, subo version 0.0.1

--- TrelloV1/js/trello.js
+++ TrelloV1/js/trello.js
@@ -2,15 +2,38 @@
 un input y un botón (formulario) para que el usuario ingrese el nombre de la lista.*/
 
 
+var btnAgregar = document.getElementById("btnAgregar");
+btnAgregar.addEventListener("click",agregarLista);
 
-function ingresarLista(){
+var lista = document.getElementById("lista");
+
+function agregarLista(){
 	var inputNombre = document.createElement("input");
-	inputNombre.setAttribute("placeholder","Nombre lista");
+	inputNombre.setAttribute("placeholder","Añadir lista...");
 	var btnEnviar = document.createElement("button");
 	btnEnviar.setAttribute("type","button");
 	btnEnviar.setAttribute("class","btn btn-success");
-	btnEnviar.innerHTML = "Guardar"
+	btnEnviar.textContent = "Guardar";
+	lista.appendChild(inputNombre);
+	lista.appendChild(btnEnviar);
 
-	document.getElementById("lista").appendChild(inputNombre);
-	document.getElementById("lista").appendChild(btnEnviar);
+	btnEnviar.addEventListener("click",agregarTarjeta);
+
+	function agregarTarjeta(){
+		var titulo = document.createElement("h2");
+		titulo.innerHTML = inputNombre.value;
+		var divTarjeta = document.createElement("div");
+		divTarjeta.appendChild(titulo);
+		lista.appendChild(divTarjeta);
+
+		
+		var inputTarjeta = document.createElement("input");
+		inputTarjeta.setAttribute("type","button");
+		inputTarjeta.setAttribute("class","btn btn-secondary transparente");
+		inputTarjeta.setAttribute("value","Añadir tarjeta...")
+		divTarjeta.appendChild(inputTarjeta);
+
+		var txtTarjeta = document.createElement("textarea");
+	}
+	
 }
