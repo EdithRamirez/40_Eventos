b95569a508de3a8dad9e34a4a8115ec9083f02fe
EdithRamirez <ediths.ramirez07@gmail.com>
2016-10-25 16:54:10 -0500

    subo modificacion de archivo de trello, ya grega mas listas

--- TrelloV1/js/trello.js
+++ TrelloV1/js/trello.js
@@ -5,16 +5,21 @@ btnAgregar.addEventListener("click",agregarLista);
 var lista = document.getElementById("lista");
 //llama a la funcion agregarLista cuando se da click en el boton
 function agregarLista(){
-	//Crea el elemento input y el boton de guardar
+	//Crea el elemento input con sus atributos
 	var inputNombre = document.createElement("input");
 	inputNombre.setAttribute("placeholder","Añadir lista...");
+	//Crea el elemento boton con sus atributos
 	var btnEnviar = document.createElement("button");
 	btnEnviar.setAttribute("type","button");
 	btnEnviar.setAttribute("class","btn btn-success");
 	btnEnviar.textContent = "Guardar";
-	//input y boton son hijos de lista
-	lista.appendChild(inputNombre);
-	lista.appendChild(btnEnviar);
+	//Se crea la caja 
+	var divCaja = document.createElement("div");
+	divCaja.setAttribute("class","caja")
+	divCaja.appendChild(inputNombre);
+	divCaja.appendChild(btnEnviar);
+	lista.appendChild(divCaja);
+
 	//llama a la funcion agregarTarjeta cuando se da click en el boton
 	btnEnviar.addEventListener("click",agregarTarjeta);	
 	function agregarTarjeta(){
@@ -22,33 +27,43 @@ function agregarLista(){
 		if(inputNombre.value == ""||inputNombre.value == null){
 			alert("Debe ingresar un titulo");
 		}else{
+			//elimina el input y el boton
+			inputNombre.parentNode.removeChild(inputNombre);
+			btnEnviar.parentNode.removeChild(btnEnviar);
+			
 			var titulo = document.createElement("h4");
 			titulo.innerHTML = inputNombre.value;
-			var divTarjeta = document.createElement("div");
-			divTarjeta.appendChild(titulo);
-			lista.appendChild(divTarjeta);
+
+			divCaja.appendChild(titulo);
 
 			var btnTarjeta = document.createElement("input");
 			btnTarjeta.setAttribute("type","button");
-			btnTarjeta.setAttribute("class","btn transparente");
+			btnTarjeta.setAttribute("class","btn btn-opaco");
 			btnTarjeta.setAttribute("value","Añadir tarjeta...")
-			divTarjeta.appendChild(btnTarjeta);
+			divCaja.appendChild(btnTarjeta);
+
 			
 			btnTarjeta.addEventListener("click",crearTarjeta);
-
+			
 			function crearTarjeta(){
-				var txtTarjeta = document.createElement("textarea");
 
+				var txtTarjeta = document.createElement("textarea");
 				var btnAnadir = document.createElement("button");
 				btnAnadir.setAttribute("type","button");
 				btnAnadir.setAttribute("class","btn btn-success");
 				btnAnadir.textContent = "Añadir";
-				divTarjeta.appendChild(txtTarjeta);
-				divTarjeta.appendChild(btnAnadir);
+				divCaja.appendChild(txtTarjeta);
+				divCaja.appendChild(btnAnadir);
 				//aqui debe guardar la tarjeta
+				btnAnadir.addEventListener("click",anadirTarjeta);
+				function anadirTarjeta(){	
+					var tarea = document.createElement("p");
+					tarea.innerHTML = txtTarjeta.value;
+					txtTarjeta.setAttribute("class","");
+					divCaja.appendChild(tarea);
+					txtTarjeta.value = "";
+				}
 			}
-
 		}
-	
 	}
 }
